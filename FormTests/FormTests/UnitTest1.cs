585a5cc2f953e7a8abfcdba8d759cb66408318b1
madziulek <madziulek343@gmail.com>
2020-08-18 18:03:25 +0200

    empty project with driver

---
+++ FormTests/FormTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace FormTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
