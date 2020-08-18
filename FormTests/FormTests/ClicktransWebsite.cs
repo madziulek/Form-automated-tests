f29545506680bc82d5b348eeb4bf777860382b5b
madziulek <madziulek343@gmail.com>
2020-08-18 20:13:49 +0200

    cleanup the code

--- FormTests/FormTests/ClicktransWebsite.cs
+++ FormTests/FormTests/ClicktransWebsite.cs
@@ -1,14 +1,11 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
-using System;
-using System.Threading;
 using Xunit;
 
 namespace FormTests
 {
-    public class Clicktrans
+    public class ClicktransWebsite
     {
         [Fact]
         public void FillingTheForm()
@@ -23,18 +20,15 @@ namespace FormTests
                 var email = driver.FindElement(By.Id("user_register_email"));
                 email.SendKeys("test@gmail.com");
                
-
                 var nameAndSurname = driver.FindElement(By.Id("user_register_name"));
-                nameAndSurname.SendKeys("Magdalena Mazur");
+                nameAndSurname.SendKeys("Magdalena Test");
                 
-
                 var country = new SelectElement(driver.FindElement(By.Id("user_register_phoneCode")));
                 country.SelectByValue("49");
 
                 var phone = driver.FindElement(By.Id("user_register_phone"));
-                phone.SendKeys("506458207");
+                phone.SendKeys("123456789");
                 
-
                 var password = driver.FindElement(By.Id("user_register_plainPassword"));
                 password.SendKeys("Magdalena1234");
 
@@ -50,13 +44,6 @@ namespace FormTests
                 var currentMessage = driver.FindElement(By.CssSelector("[class='ui success message']"));
 
                 Assert.Equal("OK - some registration logic is mocked", currentMessage.Text);
-
-
-
-
-
-
-
             }
         }
     }
